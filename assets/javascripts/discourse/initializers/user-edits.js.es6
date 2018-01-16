0f6615680e259d53576160775d483b27d67a1d9f
Angus McLeod <angus@mcleod.org.au>
2018-01-16 12:00:49 +0800

    init

---
+++ assets/javascripts/discourse/initializers/user-edits.js.es6
@@ -0,0 +1,103 @@
+import InviteController from 'discourse/controllers/invite';
+import { emailValid } from 'discourse/lib/utilities';
+import { default as computed, observes } from 'ember-addons/ember-computed-decorators';
+import { withPluginApi } from 'discourse/lib/plugin-api';
+
+export default {
+  name: 'civically-user-edits',
+  initialize(container) {
+    const currentUser = container.lookup('current-user:main');
+
+    if (currentUser && currentUser.staff) {
+      const AdminUser = requirejs('admin/models/admin-user').default;
+
+      withPluginApi('0.8.12', api => {
+        api.modifyClass('route:admin-users-list-show', {
+          model(params) {
+            this.userFilter = params.filter;
+            let opts = {};
+            const user = this.currentUser;
+            if (!user.admin) {
+              opts['custom_fields'] = {
+                place_category_id: user.moderator_category_id
+              };
+            }
+            return AdminUser.findAll(params.filter, opts);
+          },
+
+          setupController: function(controller, model) {
+            const isAdmin = this.currentUser.get('admin');
+            controller.setProperties({
+              model: model,
+              query: this.userFilter,
+              showEmails: !isAdmin,
+              refreshing: false,
+            });
+          }
+        });
+
+        api.modifyClass('controller:admin-users-list-show', {
+          @observes('order', 'ascending')
+          _refreshUsers: function() {
+            this.set('refreshing', true);
+            let opts = {
+              filter: this.get('listFilter'),
+              order: this.get('order'),
+              ascending: this.get('ascending')
+            };
+
+            const user = this.currentUser;
+            if (user.admin) {
+              opts['show_emails'] = this.get('showEmails');
+            } else {
+              opts['custom_fields'] = {
+                place_category_id: user.moderator_category_id
+              };
+            }
+
+            AdminUser.findAll(this.get('query'), opts).then( (result) => {
+              this.set('model', result);
+            }).finally( () => {
+              this.set('refreshing', false);
+            });
+          },
+        });
+      });
+    }
+
+
+    // overriding method to pass title to translations when inviting new users from a topic.
+    InviteController.reopen({
+      @computed('isMessage', 'invitingToTopic', 'emailOrUsername', 'isPrivateTopic', 'isAdmin', 'canInviteViaEmail')
+      inviteInstructions(isMessage, invitingToTopic, emailOrUsername, isPrivateTopic, isAdmin, canInviteViaEmail) {
+        if (!canInviteViaEmail) {
+          // can't invite via email, only existing users
+          return I18n.t('topic.invite_reply.sso_enabled');
+        } else if (isMessage) {
+          // inviting to a message
+          return I18n.t('topic.invite_private.email_or_username');
+        } else if (invitingToTopic) {
+          // inviting to a private/public topic
+          if (isPrivateTopic && !isAdmin) {
+            // inviting to a private topic and is not admin
+            return I18n.t('topic.invite_reply.to_username', {topicTitle: this.get('model.title')});
+          } else {
+            // when inviting to a topic, display instructions based on provided entity
+            if (Ember.isEmpty(emailOrUsername)) {
+              return I18n.t('topic.invite_reply.to_topic_blank', {topicTitle: this.get('model.title')});
+            } else if (emailValid(emailOrUsername)) {
+              this.set("inviteIcon", "envelope");
+              return I18n.t('topic.invite_reply.to_topic_email', {topicTitle: this.get('model.title')});
+            } else {
+              this.set("inviteIcon", "hand-o-right");
+              return I18n.t('topic.invite_reply.to_topic_username', {topicTitle: this.get('model.title')});
+            }
+          }
+        } else {
+          // inviting to forum
+          return I18n.t('topic.invite_reply.to_forum');
+        }
+      }
+    });
+  }
+};
---
+++ plugin.rb
@@ -0,0 +1,148 @@
+# name: civically-user
+# about: User edits for Civically
+# version: 0.1
+# authors: Angus McLeod
+# url: https://github.com/civicallyhq/civically-user
+
+after_initialize do
+  DiscoursePluginRegistry.serialized_current_user_fields << "institution"
+  DiscoursePluginRegistry.serialized_current_user_fields << "position"
+  add_to_serializer(:current_user, :institution) { object.custom_fields["institution"] }
+  add_to_serializer(:current_user, :position) { object.custom_fields["position"] }
+
+  require_dependency 'guardian'
+  Guardian.class_eval do
+    def can_invite_to_forum?(groups = nil)
+      authenticated? &&
+      (SiteSetting.max_invites_per_day.to_i > 0 || is_staff?) &&
+      !SiteSetting.enable_sso &&
+      SiteSetting.enable_local_logins &&
+      (
+        (!SiteSetting.must_approve_users? && @user.has_trust_level?(TrustLevel[1])) ||
+        is_staff?
+      ) &&
+      (groups.blank? || is_admin?)
+    end
+
+    def can_invite_to?(object, groups = nil)
+      return false unless authenticated?
+      return true if is_admin?
+      return false unless SiteSetting.enable_private_messages?
+      return false if (SiteSetting.max_invites_per_day.to_i == 0 && !is_staff?)
+      return false unless can_see?(object)
+      return false if groups.present?
+
+      if object.is_a?(Topic) && object.category
+        if object.category.groups.any?
+          return true if object.category.groups.all? { |g| can_edit_group?(g) }
+        end
+      end
+
+      user.has_trust_level?(TrustLevel[1])
+    end
+
+    def can_delete_user?(user)
+      return false if user.nil? || user.admin?
+      if is_me?(user)
+        user.post_count <= 1
+      else
+        is_admin? && (user.first_post_created_at.nil? || user.first_post_created_at > SiteSetting.delete_user_max_post_age.to_i.days.ago)
+      end
+    end
+
+    def can_delete_all_posts?(user)
+      is_admin? &&
+      user &&
+      !user.admin? &&
+      (user.first_post_created_at.nil? || user.first_post_created_at >= SiteSetting.delete_user_max_post_age.days.ago) &&
+      user.post_count <= SiteSetting.delete_all_posts_max.to_i
+    end
+
+    def can_change_trust_level?(user)
+      user && is_admin?
+    end
+  end
+
+  if Rails.env.development?
+    SiteSetting.port = 3000
+    SiteSetting.bootstrap_mode_enabled = false
+    SiteSetting.show_create_topics_notice = false
+  end
+
+  unless PluginStoreRow.exists?(plugin_name: 'custom_wizard', key: 'welcome')
+    CustomWizard::Wizard.add_wizard(File.read(File.join(
+      Rails.root, 'plugins', 'civically-user', 'config', 'wizards', 'welcome.json'
+    )))
+  end
+
+  Admin::AdminController.class_eval do
+    def ensure_admin
+      raise Discourse::InvalidAccess.new unless current_user && current_user.admin?
+    end
+  end
+
+  Admin::StaffActionLogsController.class_eval do
+    before_action :ensure_admin
+  end
+
+  Admin::ScreenedEmailsController.class_eval do
+    before_action :ensure_admin
+  end
+
+  Admin::ScreenedIpAddressesController.class_eval do
+    before_action :ensure_admin
+  end
+
+  Admin::ScreenedUrlsController.class_eval do
+    before_action :ensure_admin
+  end
+
+  Admin::WatchedWordsController.class_eval do
+    before_action :ensure_admin
+  end
+
+  Admin::SearchLogsController.class_eval do
+    before_action :ensure_admin
+  end
+
+  Admin::PluginsController.class_eval do
+    before_action :ensure_admin
+  end
+
+  module AdminFindUsersQueryExtension
+    def filter_by_custom_fields
+      if params[:custom_fields].present?
+        params[:custom_fields].each do |k, v|
+          @query = @query.where("users.id in (
+            SELECT user_id FROM user_custom_fields WHERE name = '#{k}' AND value = ?
+          )", v)
+        end
+      end
+      @query
+    end
+
+    def find_users_query
+      append filter_by_custom_fields
+      super
+    end
+  end
+
+  require_dependency 'admin_user_index_query'
+  class ::AdminUserIndexQuery
+    prepend AdminFindUsersQueryExtension
+    SORTABLE_MAPPING['place_category_id'] = "(SELECT value FROM user_custom_fields WHERE user_id = users.id AND name = 'place_category_id' LIMIT 1)"
+  end
+
+  Admin::UsersController.class_eval do
+    def index
+      users = ::AdminUserIndexQuery.new(params).find_users
+
+      if current_user.admin? && params[:show_emails] == "true"
+        guardian.can_see_emails = true
+        StaffActionLogger.new(current_user).log_show_emails(users)
+      end
+
+      render_serialized(users, AdminUserListSerializer)
+    end
+  end
+end
