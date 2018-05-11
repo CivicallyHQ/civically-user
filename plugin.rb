dd77c06e0319461b49c09176f0ae85fd60124440
Angus McLeod <angus@mcleod.org.au>
2018-05-11 13:05:13 +1000

    Add make a post as final getting started checklist item

--- plugin.rb
+++ plugin.rb
@@ -67,9 +67,7 @@ after_initialize do
   DiscourseEvent.on(:user_created) do |user|
     CivicallyUser::User.create_checklist(user)
     CivicallyApp::App.add(user, 'civically-user', enabled: true, widget: { position: 'right', order: 0 })
-    CivicallyApp::App.add(user, 'civically-site', enabled: false, widget: { position: 'right', order: 1 })
     CivicallyApp::App.add(user, 'civically-navigation', enabled: false, widget: { position: 'left', order: 0 })
-    CivicallyApp::App.add(user, 'civically-place', enabled: false, widget: { position: 'left', order: 1 })
   end
 
   require_dependency 'application_controller'
